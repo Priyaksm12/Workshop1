c6691104bbe4bc96a7bb910f4ac4ea82b8206ac5
Priyaksm12 <94815020+Priyaksm12@users.noreply.github.com>
2021-12-22 08:40:37 +0530

    Update Gambling.java

--- src/Gambling.java
+++ src/Gambling.java
@@ -3,6 +3,9 @@ public class Gambling {
         public static void main(String[] args){
             final int STARTING_BET_STAKE = 100;
             final int MINIMUM_BET_PER_GAME = 1;
+            System.out.println(STARTING_BET_STAKE);
+            System.out.println(MINIMUM_BET_PER_GAME);
         }
+        
     }
 
