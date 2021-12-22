a2c643ccc77bbde58e48b694a4c329fe4a95a31f
Priyaksm12 <priyankaks.priya@gmail.com>
2021-12-22 08:31:58 +0530

    Uc1

---
+++ src/Gambling.java
@@ -0,0 +1,8 @@
+public class Gambling {
+
+        public static void main(String[] args){
+            final int STARTING_BET_STAKE = 100;
+            final int MINIMUM_BET_PER_GAME = 1;
+        }
+    }
+
